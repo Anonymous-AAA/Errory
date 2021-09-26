9204b92e0a337b342c273a6d26d6d48f6f2ebc40
Anonymous-AAA <77876851+Anonymous-AAA@users.noreply.github.com>
2021-09-26 15:51:23 +0530

    Update errory.py
    Version 0.2

--- errory.py
+++ errory.py
@@ -12,6 +12,14 @@ tst.extractall("./archive")
 
 subprocess.run("mkdir compile",shell=True)
 subprocess.run("mkdir out",shell=True)
+r="error_"+sys.argv[1][0:6]
+subprocess.run("mkdir "+r,shell=True)
+fc=open("./"+r+"/compile_error_"+sys.argv[1][0:6]+".txt",'a')
+fr=open("./"+r+"/runtime_error_"+sys.argv[1][0:6]+".txt",'a')
+ft=open("./"+r+"/testcase_mismatch_"+sys.argv[1][0:6]+".txt",'a')
+fc.truncate(0)
+fr.truncate(0)
+ft.truncate(0)
 
 
 
@@ -19,19 +27,24 @@ for files in os.listdir("./"+sys.argv[1][:-4]):
     l=subprocess.run("gcc "+sys.argv[1][:-4]+"/"+files+" -o ./compile/"+files[:-2],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     if l.stderr.decode("utf-8")!="":
         str=l.stderr.decode("utf-8")
-        fc=open("compile_error_"+sys.argv[1][0:6]+".txt",'a')
+        
         fc.write(files+"\n\n")
         fc.write(str+"\n\n\n")
 
+fc.close()
 
 for files in os.listdir("./compile"):
     subprocess.run("mkdir ./out/q"+files[-1],shell=True)
     for  foldername,subfolders,filenames in os.walk("./archive"):
         for fil in filenames:
             if re.match(reg,fil) and os.path.basename(foldername)=="q"+files[-1]:
-                subprocess.run("./compile/"+files+"<"+foldername.replace(" ", "\\ ")+"/"+fil+">./out/q"+files[-1]+"/out"+fil[2]+".txt",shell=True)
+                m=subprocess.run("./compile/"+files+"<"+foldername.replace(" ", "\\ ")+"/"+fil+">./out/q"+files[-1]+"/out"+fil[2]+".txt",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+                if m.stderr.decode("utf-8")!="":
+                        
+                        fr.write(os.path.basename(foldername)+" TestCase"+fil[-5]+"\n")
+                        fr.write(m.stderr.decode("utf-8")+"\n\n")
 
-
+fr.close()
 
 for  foldername,subfolders,filenames in os.walk("./out"):
     for filename in filenames:
@@ -40,9 +53,12 @@ for  foldername,subfolders,filenames in os.walk("./out"):
                 if fil==filename and os.path.basename(folder)==os.path.basename(foldername):
                     k=subprocess.run("diff -Z -B  "+foldername+"/"+filename+" "+folder.replace(" ", "\\ ")+"/"+fil,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
                     if k.stdout.decode("utf-8")!="":
-                        ft=open("testcase_mismatch_"+sys.argv[1][0:6]+".txt",'a')
+                        
                         ft.write(os.path.basename(foldername)+" TestCase"+fil[-5]+"\n")
                         ft.write(k.stdout.decode("utf-8")+"\n\n")
+
+ft.close()
+
 try:
      if sys.argv[3]=='d':
         subprocess.run("rm -r archive compile out "+sys.argv[1][:-4],shell=True)
